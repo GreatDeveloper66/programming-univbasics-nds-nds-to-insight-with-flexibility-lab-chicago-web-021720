37d069f9e0ce4838098ed1d1f4abe3822fc6548c
cerebral-caller-0985 <ashaffe@gmail.com>
2020-01-29 16:51:44 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -90,7 +90,6 @@ def movies_with_directors_set(source)
   #
   # Array of Arrays containing all of a director's movies. Each movie will need
   # to have a :director_name key added to it.
-  binding.pry
   retarr = []
   source2 = source.each do |movies|
     name = movies[:name]
@@ -98,6 +97,7 @@ def movies_with_directors_set(source)
       retarr.push({director_name: name, movies: movie})
     end
   end
+  binding.pry
 
   #puts "beg"
   #puts retarr
